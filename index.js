965edae0d0419e3217cc7421e00b14321feb063d
Mateico <matimercante@gmail.com>
2022-12-06 10:26:55 -0300

    first commit

--- index.js
+++ index.js
@@ -25,6 +25,24 @@ const cosasQueAprendimos = [
   },
 ];
 
-function main() {}
+
+
+function main() {
+  const listaFinal = document.querySelector(".lista")
+  const listaEl = document.querySelectorAll(".lista li")
+  for (let el of listaEl){
+    el.remove()
+  }
+  for (let i of cosasQueAprendimos){
+    const newLi = document.createElement("li")
+    newLi.textContent = i.tema
+    newLi.classList.add(i.class || "item")
+    console.log(newLi)
+    listaFinal.appendChild(newLi)
+
+  }
+  
+  
+}
 
 main();
